3969194316771fe98ccd408a38e434c0bc3e7585
Elad Gubkin <Eladox3@gmail.com>
2019-10-16 23:10:35 +0300

    rm search input from header

--- frontend/src/components/layout/header/Header.jsx
+++ frontend/src/components/layout/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import PropTypes from 'prop-types';
@@ -8,69 +8,49 @@ import viewAgendaSvg from '../../../assets/svg/view-agenda.svg';
 import magnifySvg from '../../../assets/svg/magnify.svg';
 import { connect } from 'react-redux';
 import { logout } from '../../../redux/ducks/auth/actions';
+import { toggleSearchComponent } from '../../../redux/ducks/components/actions';
 
 const Header = props => {
   const { toggleDrawer } = props;
-  const [searchValue, setSearchValue] = useState('');
+  // const [searchValue, setSearchValue] = useState('');
   const classes = useStyles();
 
-  const onChange = e => {
-    setSearchValue(e.target.value);
-    console.log('Search!');
-  };
+  // const onChange = e => {
+  //   setSearchValue(e.target.value);
+  //   console.log('Search!');
+  // };
 
   return (
-    <div className={classes.grow}>
-      <AppBar position="static" id="Header">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-            onClick={toggleDrawer('left', true)}
-          >
-            <MenuIcon />
+    <AppBar position="static" id="Header">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="open drawer"
+          onClick={toggleDrawer('left', true)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography className={classes.title} variant="h6" noWrap>
+          myguitartabs
+        </Typography>
+        <div className={classes.grow} />
+        <div>
+          <IconButton color="inherit" onClick={props.toggleSearchComponent}>
+            <img src={magnifySvg} alt="magnifySvg" />
           </IconButton>
-          <Typography className={classes.title} variant="h6" noWrap>
-            myguitartabs
-          </Typography>
-          {/* <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search Tabs…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-              onChange={onChange}
-              value={searchValue}
-            />
-          </div> */}
-          <div className={classes.grow} />
-          <div>
-            <IconButton aria-label="show more" aria-haspopup="true" color="inherit">
-              <img src={magnifySvg} alt="magnifySvg" />
-            </IconButton>
-            <IconButton aria-label="show more" aria-haspopup="true" color="inherit">
-              <img src={viewAgendaSvg} alt="viewAgendaSvg" />
-            </IconButton>
-            <IconButton
-              aria-label="show more"
-              aria-haspopup="true"
-              color="inherit"
-              onClick={props.logout}
-            >
-              {/* <MoreIcon /> */}
-              <img src={accountCircleSvg} alt="accountCircleSvg" />
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
+
+          <IconButton aria-label="show more" aria-haspopup="true" color="inherit">
+            <img src={viewAgendaSvg} alt="viewAgendaSvg" />
+          </IconButton>
+
+          <IconButton color="inherit" onClick={props.logout}>
+            <img src={accountCircleSvg} alt="accountCircleSvg" />
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 };
 
@@ -78,7 +58,8 @@ Header.propTypes = {
   toggleDrawer: PropTypes.func.isRequired,
   left: PropTypes.bool.isRequired,
   auth: PropTypes.object.isRequired,
-  logout: PropTypes.func.isRequired
+  logout: PropTypes.func.isRequired,
+  toggleSearchComponent: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -87,5 +68,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { logout }
+  { logout, toggleSearchComponent }
 )(Header);
