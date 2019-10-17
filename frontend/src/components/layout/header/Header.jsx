d9fd453058ebd2d6f8ba95eeb7318c80612d124b
Elad Gubkin <Eladox3@gmail.com>
2019-10-17 20:52:41 +0300

    style for Header, logic for sidebar

--- frontend/src/components/layout/header/Header.jsx
+++ frontend/src/components/layout/header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import useStyles from '../../../hooks/useStyles';
 import accountCircleSvg from '../../../assets/svg/account-circle.svg';
 import viewAgendaSvg from '../../../assets/svg/view-agenda.svg';
 import magnifySvg from '../../../assets/svg/magnify.svg';
@@ -10,9 +11,29 @@ import { connect } from 'react-redux';
 import { logout } from '../../../redux/ducks/auth/actions';
 import { toggleSearchComponent } from '../../../redux/ducks/components/actions';
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  }
+}));
+
 const Header = props => {
-  const { toggleDrawer } = props;
+  const { toggleDrawer, left } = props;
   // const [searchValue, setSearchValue] = useState('');
+
   const classes = useStyles();
 
   // const onChange = e => {
@@ -21,21 +42,38 @@ const Header = props => {
   // };
 
   return (
-    <AppBar position="static" id="Header">
+    <AppBar
+      id="Header"
+      position="fixed"
+      className={
+        window.innerWidth > 1024
+          ? clsx(classes.appBar, {
+              [classes.appBarShift]: left
+            })
+          : null
+      }
+    >
       <Toolbar>
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="open drawer"
-          onClick={toggleDrawer('left', true)}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography className={classes.title} variant="h6" noWrap>
+        {left ? null : (
+          <IconButton
+            edge="start"
+            className="menu-button"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer('left', true)}
+          >
+            <MenuIcon />
+          </IconButton>
+        )}
+
+        {/* <IconButton color="inherit" className="logo-img">
+          <img src={svgGuitarAccoustic} alt="svgGuitarAccoustic" />
+        </IconButton> */}
+
+        <Typography className="logo" variant="h6" noWrap>
           myguitartabs
         </Typography>
-        <div className={classes.grow} />
+        <div className="grow" />
         <div>
           <IconButton color="inherit" onClick={props.toggleSearchComponent}>
             <img src={magnifySvg} alt="magnifySvg" />
