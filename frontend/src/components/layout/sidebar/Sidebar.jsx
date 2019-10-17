326b53881639dd2fe3cce1b540dbe91895e01c95
Elad Gubkin <Eladox3@gmail.com>
2019-10-17 20:53:22 +0300

    style for Sidebar, add presistent drawer

--- frontend/src/components/layout/sidebar/Sidebar.jsx
+++ frontend/src/components/layout/sidebar/Sidebar.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { SwipeableDrawer } from '@material-ui/core';
 import SidebarContent from './SidebarContent';
 import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   list: {
     width: 250
   },
   fullList: {
     width: 'auto'
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    background: '#f5f5f5',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end'
   }
-});
+}));
 
 const Sidebar = props => {
   const { toggleDrawer, left } = props;
   const classes = useStyles();
+  const theme = useTheme();
   const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
 
   return (
     <SwipeableDrawer
+      id="Sidebar"
       open={left}
+      variant={window.innerWidth > 1024 ? 'persistent' : 'temporary'}
       onClose={toggleDrawer('left', false)}
       onOpen={toggleDrawer('left', true)}
       disableBackdropTransition={!iOS}
       disableDiscovery={iOS}
     >
+      <div className={classes.drawerHeader}>
+        <IconButton onClick={toggleDrawer('left', false)}>
+          {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+        </IconButton>
+      </div>
       <SidebarContent
         side="left"
         classes={classes}
