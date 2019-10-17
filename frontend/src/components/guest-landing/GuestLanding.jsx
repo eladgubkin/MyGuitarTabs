147a6ef88d1189a4b163443f31491a944bd71153
Elad Gubkin <Eladox3@gmail.com>
2019-10-17 20:57:56 +0300

    redirect to /home/ if authenticated

--- frontend/src/components/guest-landing/GuestLanding.jsx
+++ frontend/src/components/guest-landing/GuestLanding.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import Nav from './Nav';
 import TypeWriter from './TypeWriter';
 import Presentation from './Presentation';
 import Button from './Button';
 
-const GuestLanding = () => {
+const GuestLanding = props => {
+  if (props.isAuthenticated) return <Redirect to="/home/" />;
+
   return (
     <div id="GuestLanding">
       <div className="container">
@@ -17,4 +22,15 @@ const GuestLanding = () => {
   );
 };
 
-export default GuestLanding;
+GuestLanding.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(GuestLanding);
