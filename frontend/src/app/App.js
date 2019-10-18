4b129a55cc697689aaf4721709117e39d372c19d
Elad Gubkin <Eladox3@gmail.com>
2019-10-18 23:00:56 +0300

    fix loading and rm Provider

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from '../redux/store';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { loadUser } from '../redux/ducks/auth/actions';
 import '../assets/scss/main.scss';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 // Componenets
+import Loading from '../components/common/Loading';
 import PrivateRoute from '../components/common/PrivateRoute';
 import Register from '../components/authentication/Register';
 import Login from '../components/authentication/Login';
@@ -15,29 +16,39 @@ import Search from '../components/auth-landing/Search';
 import NotFoundPage from '../components/common/NotFoundPage';
 import useComponentDidMount from '../hooks/useComponentDidMount';
 
-const App = () => {
-  useComponentDidMount(() => store.dispatch(loadUser()));
+const App = props => {
+  const { isLoading } = props.auth;
 
-  const { isLoading } = store.getState().auth;
+  useComponentDidMount(() => props.loadUser());
 
   if (isLoading) {
-    return <h1>Loading...</h1>;
+    return <Loading />;
   } else {
     return (
-      <Provider store={store}>
-        <Router>
-          <Switch>
-            <PrivateRoute exact path="/home/" component={AuthLanding} />
-            <PrivateRoute exact path="/search/" component={Search} />
-            <Route exact path="/" component={GuestLanding} />
-            <Route exact path="/register/" component={Register} />
-            <Route exact path="/login/" component={Login} />
-            <Route exact path="*" component={NotFoundPage} />
-          </Switch>
-        </Router>
-      </Provider>
+      <Router>
+        <Switch>
+          <PrivateRoute exact path="/home/" component={AuthLanding} />
+          <PrivateRoute exact path="/search/" component={Search} />
+          <Route exact path="/" component={GuestLanding} />
+          <Route exact path="/register/" component={Register} />
+          <Route exact path="/login/" component={Login} />
+          <Route exact path="*" component={NotFoundPage} />
+        </Switch>
+      </Router>
     );
   }
 };
 
-export default App;
+App.propTypes = {
+  auth: PropTypes.object.isRequired,
+  loadUser: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { loadUser }
+)(App);
