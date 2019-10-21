5b78518eeb0219c19fa22859e07ec4a975cdf28e
Elad Gubkin <Eladox3@gmail.com>
2019-10-21 22:30:29 +0300

    rm <Redirect to="/home/" />

--- frontend/src/components/guest-landing/GuestLanding.jsx
+++ frontend/src/components/guest-landing/GuestLanding.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 import Nav from './Nav';
 import TypeWriter from './TypeWriter';
 import Presentation from './Presentation';
 import Button from './Button';
 
 const GuestLanding = props => {
-  if (props.isAuthenticated) return <Redirect to="/home/" />;
-
   return (
     <div id="GuestLanding">
       <div className="container">
