cbcdceca087cd904eaa5337a02c93994edd5690e
Elad Gubkin <Eladox3@gmail.com>
2019-10-25 23:24:36 +0300

    import Label and association

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,7 +1,4 @@
 from backend.models.User import *
 from backend.models.Song import *
-
-# favorites = db.Table('favorites',
-#                      db.Column('user_id', db.String(32),
-#                                db.ForeignKey('users.user_id')),
-#                      db.Column('song_id', db.String(32), db.ForeignKey('songs.song_id')))
+from backend.models.Label import *
+from backend.models.association import *
