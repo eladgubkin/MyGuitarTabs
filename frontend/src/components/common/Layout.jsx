b5eeb5cf074f4d01c2baa0e854d4c68abc668feb
Elad Gubkin <Eladox3@gmail.com>
2019-10-25 23:28:31 +0300

    move Layout to common

---
+++ frontend/src/components/common/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Components
+import Header from '../header/Header';
+import Sidebar from '../sidebar/Sidebar';
+
+const Layout = props => {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+    </>
+  );
+};
+
+export default Layout;
