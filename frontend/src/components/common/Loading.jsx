07c50ef2ca8307c1548c23c6411d5c443ff08c89
Elad Gubkin <Eladox3@gmail.com>
2019-10-27 21:45:19 +0200

    add circular loading

--- frontend/src/components/common/Loading.jsx
+++ frontend/src/components/common/Loading.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const Loading = () => {
   return (
     <div id="Loading">
-      <h1>Loading...</h1>
+      <CircularProgress size={60} />;{/* <h1>Loading...</h1> */}
     </div>
   );
 };
