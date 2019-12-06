a3106a9d86a634d927a88fd055e9051e7d701b1d
Elad Gubkin <Eladox3@gmail.com>
2019-12-06 02:39:55 +0200

    add labels

--- frontend/src/redux/ducks/index.js
+++ frontend/src/redux/ducks/index.js
@@ -2,3 +2,4 @@ export { default as songs } from './songs';
 export { default as auth } from './authentication';
 export { default as errors } from './errors';
 export { default as components } from './components';
+export { default as labels } from './labels';
